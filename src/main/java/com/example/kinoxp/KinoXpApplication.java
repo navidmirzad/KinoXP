ea91dc8e8ca7d9fca1ac66becbd262540052a4f3
navidmirzad <navi0221@stud.kea.dk>
2023-09-28 12:27:32 +0200

    created packages + started on ExceptionHandler

--- src/main/java/com/example/kinoxp/KinoXpApplication.java
+++ src/main/java/com/example/kinoxp/KinoXpApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KinoXpApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KinoXpApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KinoXpApplication.class, args);
+    }
 
 }
