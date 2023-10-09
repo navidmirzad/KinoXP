80a7cb9f1b90b7a08305229f2b0937499d1e5bb8
math92n4 <math92n4@stud.kea.dk>
2023-10-09 13:33:03 +0200

    post customer

--- src/main/java/com/example/kinoxp/controller/CustomerController.java
+++ src/main/java/com/example/kinoxp/controller/CustomerController.java
@@ -1,5 +1,6 @@
 package com.example.kinoxp.controller;
 
+import com.example.kinoxp.dto.PostCustomerDTO;
 import com.example.kinoxp.model.Customer;
 import com.example.kinoxp.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @CrossOrigin
@@ -29,6 +31,31 @@ public class CustomerController {
             }
         }
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+    @GetMapping("/customers")
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
+    }
+
+    @PostMapping("/kinoxp/createaccount")
+    public ResponseEntity<Customer> createAccount (@RequestBody PostCustomerDTO customerDTO) {
+        System.out.println("kommer jeg herind??");
+        Customer customer = new Customer();
+        customer.setFirstName(customerDTO.getFirstName());
+        customer.setLastName(customerDTO.getLastName());
+        customer.setEmail(customerDTO.getEmail());
+        customer.setUserName(customerDTO.getUserName());
+        customer.setPassword(customerDTO.getPassword());
+
+        Optional<Customer> existingCustomerEmail = customerRepository.findCustomerByEmail(customer.getEmail());
+        Optional<Customer> existingCustomerUsername = customerRepository.findCustomerByUserName(customer.getEmail());
+        if (existingCustomerEmail.isPresent() || existingCustomerUsername.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        } else {
+            customerRepository.save(customer);
+            return new ResponseEntity<>(customer, HttpStatus.CREATED);
+        }
     }
 
 
--- src/main/java/com/example/kinoxp/repositories/CustomerRepository.java
+++ src/main/java/com/example/kinoxp/repositories/CustomerRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-    Optional<Customer> findCustomerByEmail(String email );
+    Optional<Customer> findCustomerByEmail(String email);
+    Optional<Customer> findCustomerByUserName(String userName);
 }
